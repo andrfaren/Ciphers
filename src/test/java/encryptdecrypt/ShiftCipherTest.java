3a3b10f3a499c572f15c50dc7d39e414d5bc3b72
Andrew <97142367+andrfaren@users.noreply.github.com>
2022-03-09 13:48:36 -0500

    added unit tests to the shift cipher algorithm

---
+++ src/test/java/encryptdecrypt/ShiftCipherTest.java
@@ -0,0 +1,24 @@
+package encryptdecrypt;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShiftCipherTest {
+
+    @Test
+    void execute() {
+        ShiftCipher shiftCipherEncryption = new ShiftCipher(CipherMode.ENCRYPTION);
+        ShiftCipher shiftCipherDecryption = new ShiftCipher(CipherMode.DECRYPTION);
+
+        // Check that encryption/decryption works for obvious cases
+        assertEquals("Cpftgy", shiftCipherEncryption.execute("Andrew", 2));
+        assertEquals("Andrew", shiftCipherDecryption.execute("Cpftgy", 2));
+
+        // Check if wrong cipher mode is used
+        assertNotEquals("Cpftgy", shiftCipherDecryption.execute("Andrew", 2));
+        assertNotEquals("Andrew", shiftCipherEncryption.execute("Cpftgy", 2));
+
+
+    }
+}
